82594fefa564bb133035b39386d0edb0e4432284
misaelucas <misaelucas@gmail.com>
2022-04-06 10:30:36 -0300

    Adding images, react router and starting to build a few components, nothing robust yet.

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+      <header className="header">
+        <h2>Cabeçalho</h2>
+    </header>
+    );
+  }
+
---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Sobre</h2>
+      </main>
+    );
+  }
+
